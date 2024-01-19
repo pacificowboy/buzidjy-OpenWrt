33d41d04b23ac1db9da5eae65c27800f51976167
buzidjy <122658705+buzidjy@users.noreply.github.com>
2024-01-20 01:01:56 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -40,6 +40,8 @@ git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-a
 git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 
 # Themes
+git clone --depth=1 https://github.com/sirpdboy/luci-app-advancedplus package/luci-app-advancedplus
+git clone --depth=1 https://github.com/sirpdboy/luci-theme-kucat package/luci-theme-kucat
 git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
